f470fb11a5105edeade143e7ef8aa756ea29e31d
madhu <58720413+madhu007-cell@users.noreply.github.com>
2021-02-04 22:44:01 +0530

    Update change_permissions.sh

--- script/change_permissions.sh
+++ script/change_permissions.sh
@@ -2,7 +2,4 @@
 
 # Fix user rights
 sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+sudo chown -R ec2-user:apache /var/www/html
